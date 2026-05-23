568191ee13f346ee691b7e690eca96a21af47086
Icecream95 <the.real.icecream95@gmail.com>
2026-05-24 01:33:35 +1200

    Cleanup

--- crash.c
+++ crash.c
@@ -1,6 +1,4 @@
-#include <fcntl.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <sys/mman.h>
 #include <unistd.h>
 
@@ -12,31 +10,28 @@
 #define WAIT_STORES() asm volatile("dsb sy; isb" ::: "memory")
 #define ZVA(p)        asm volatile("dc zva, %0" :: "r"(p) : "memory")
 
-int main(void) 
+int main(void)
 {
     char *arena = mmap(NULL, 2 * 1024 * 1024, PROT_READ | PROT_WRITE,
                        MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    
+
     for (int go = 0; go < 10000; ++go) {
         printf("%d\n", go);
 
         for (int a = 0; a < ITERS; ++a) {
-            char **ref = (char **)(arena + 256);
             char *str = arena;
+            char **ref = (char **)(arena + 256);
             char *alloc = arena + 512 + a * 0x90;
-        
-            *ref = str;
+
             for (int i = 0; i < 40; ++i) {
                 str[i] = i;
             }
+            *ref = str;
             ACCESS(alloc);
-            INVALIDATE(alloc);
+
             INVALIDATE(ref);
+            INVALIDATE(alloc);
 
-            WAIT_STORES();
-
-            ACCESS(alloc - 64);
-        
             WAIT_STORES();
             // And we begin!
 
@@ -44,9 +39,9 @@ int main(void)
             BARRIER(); // should be optional
             ZVA(alloc);
             BARRIER(); // should be optional
-        
+
             const char *s = *ref;
-        
+
             for (int i = 0; i < 40; ++i) {
                 ACCESS(s + i);
             }
